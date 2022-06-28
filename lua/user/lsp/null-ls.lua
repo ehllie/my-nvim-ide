17f6b1b9bdbce2e4aa1a69d711c0726fc66b156c
Elizabeth Paź <ellieeepaz@gmail.com>
2022-06-28 07:21:02 +0200

    Changed stylua settings to use 2 spaces instead of tabs

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+  return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -10,17 +10,17 @@ local diagnostics = null_ls.builtins.diagnostics
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.prettier.with({
-			extra_filetypes = { "toml" },
-			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-		}),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.isort.with({ extra_args = { "--profile", "black" } }),
-		formatting.stylua,
-		formatting.google_java_format,
+  debug = false,
+  sources = {
+    formatting.prettier.with({
+      extra_filetypes = { "toml" },
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.isort.with({ extra_args = { "--profile", "black" } }),
+    formatting.stylua.with({ extra_args = { "--indent_type", "spaces", "--indent_width", "2" } }),
+    formatting.google_java_format,
     formatting.rustfmt,
-		diagnostics.flake8.with({ extra_args = { "--max-line-length", "--extend-ignore", "E203" } }),
-	},
+    diagnostics.flake8.with({ extra_args = { "--max-line-length", "--extend-ignore", "E203" } }),
+  },
 })
--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -1,10 +1,10 @@
 return {
-	settings = {
-		python = {
-			analysis = {
-				typeCheckingMode = "basic",
-				extraPaths = { ".local/lib/python3.10/site-packages" },
-			},
-		},
-	},
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "basic",
+        extraPaths = { ".local/lib/python3.10/site-packages" },
+      },
+    },
+  },
 }
