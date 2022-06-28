e2bcb25b3c8750d6ad6a31612be7aba9f2197941
Ellie <ellieeepaz@gmail.com>
2022-06-28 10:58:52 +0200

    Removed formatting capabilities from jsonls as it's handled by prettier via null-ls

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -47,5 +47,10 @@ for _, server in pairs(servers) do
     opts = vim.tbl_deep_extend("force", rust_analyzer_opts, opts)
   end
 
+  if server == "jsonls" then
+    local jsonls_opts = require("user.lsp.settings.jsonls")
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  end
+
   lspconfig[server].setup(opts)
 end
